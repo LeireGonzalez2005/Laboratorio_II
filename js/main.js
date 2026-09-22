61bd3aaec2895dd71d65242eab1ad8b8dfb5e532
LeireGonzalez2005 <leiregonvil0@gmail.com>
2026-09-22 15:41:13 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 // Importar clases
-
+import { Jugador} from "./jugador.js";
+import { Maquina } from "./maquina.js";
+import { Extraterrestre } from "./extraterrestre.js";
+import { Humano } from "./humano.js";
 
 // The Fisher-Yates algorithm for shuffling an array
 const shuffleArray = array => {
@@ -13,8 +16,21 @@ const shuffleArray = array => {
 
 // Inicializar un único array con 50 maquinas, 50 humanos y 50 extraterrestres
 let campo = []
+for (let i = 0; i<50; i++){
+    let maquina = new Maquina("m"+i)
+    campo.push(maquina)}
+
+for (let i = 0; i<50; i++){
+    let humano = new Humano("h"+i)
+    campo.push(humano)}
+
+for (let i = 0; i<50; i++){
+    let extraterrestre = new Extraterrestre("e"+i)
+    campo.push(extraterrestre)}
+
 
 // Ordenar los elementos del array al azar
+shuffleArray(campo)
 
 
 // Imprimir el campo
@@ -24,11 +40,19 @@ let turno =1
 
 // Mientras quede mas de un jugador en el array pelear por parejas
 // while....
+while (campo.length>1){
     console.log(`Turno: ${turno}`)
     // Pelear por parejas. Si los elementos que quedan en el array son impares, el último no pelea
     // eliminar los que se quedan fuera
     // ordernar
+    for (let i=0; i<campo.length; i++){
+        campo[i].luchar(campo[i+1])
+        i+=2
+    }
+    campo.filter(jug=>jug.salud>0)
+    shuffleArray(campo)
     turno++
+}
 
 // Imprimir campeón. Unico elemento que queda en el array
-console.log("Campeón: " + campo[0].nombre) 
+console.log("Campeón: " + campo[0].nombre)
