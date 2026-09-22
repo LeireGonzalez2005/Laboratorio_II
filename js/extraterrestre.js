1cac0b1132dfd76451523a397d7e43f8e992b772
LeireGonzalez2005 <leiregonvil0@gmail.com>
2026-09-22 15:40:13 +0200

    Update extraterrestre.js

--- js/extraterrestre.js
+++ js/extraterrestre.js
@@ -1,1 +1,10 @@
+import{Jugador} from "./jugador.js"
+export class Extraterrestre extends Jugador {
+    constructor(nombre){
+        super(nombre,Math.floor(Math.random()*(85-1+1))+1,150)  
+    }
+    get nombre(){
+        return this.miNombre.split("").reverse().join("")
+    }
+}
 
